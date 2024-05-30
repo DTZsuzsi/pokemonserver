09885b7deb219330d28965cb4c112977885bf4ad
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2024-05-30 11:51:39 +0200

    tested

--- server/model/users.js
+++ server/model/users.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   user_name: String,
   password: String,
   createdAt: Date
 });
 
-export default model('User', userSchema);
+export default model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-import express from 'express'
+import express from 'express';
 import passwords from '/Users/Zsuzsi/Documents/pokemon/server/pokemonserver/server/keys.js';
-import mongoose from 'mongoose'
-import encrypt from 'encrypt-with-password'
+import mongoose from 'mongoose';
+import User from '/Users/Zsuzsi/Documents/pokemon/server/pokemonserver/server/model/users.js'
+// import encrypt from 'encrypt-with-password';
 
-
-mongoose.connect(passwords[1].mongoconnect)
+mongoose.connect(passwords[1].mongoconnect);
 
 // ('mongodb+srv://pokeserver:rattata2@pokecluster.olee5ij.mongodb.net/')
 
-// 
+//
 const app = express();
 
 app.use(express.json());
@@ -18,27 +18,43 @@ app.listen(4444, () => console.log('Server started on port 4444'));
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 // app.use(express.static(join(__dirname, '../static')));
 
-
-app.post('/api/user', (req,res)=>{
-    const userName = req.body.user_name;
+app.post('/api/user', (req, res) => {
+  const userName = req.body.user_name;
   const password = req.body.password;
-  const encryptPassword=password.encrypt(password)
+//   const encryptPassword = password.encrypt(password);
   const createdAt = Date.now();
-  
 
-  const User = new User({
+  const user = new User({
     userName,
-    encryptPassword,
+    password,
     createdAt,
   });
-  User
-    .save()
-    .then((User) => res.json(User))
+  user.save()
+    .then((user) => res.json(user))
     .catch((err) => res.status(400).json({ success: false }));
 
-    console.log(User)
+  console.log(user);
 });
 
+app.delete('/api/users', async (req, res) => {
+  const userName = req.body;
+  try {
+    const result = await User.deleteOne({ userName });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ succes: false, message: 'User not found' });
+    }
+    res.json({ success: true, message: `${userName} deleted` });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Error deleting user' });
+  }
+});
 
-
-/
+app.get('/api/userList', async (req, res) => {
+  try {
+    const userID = req.body;
+    const User = await User.findByID(userID);
+    res.json(User);
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Error fetching user' });
+  }
+});
